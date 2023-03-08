2ccf8865eeddf074b4c1fcace1a736c7c0b1ef2d
ani <aninur.teaching@gmail.com>
2023-03-08 09:37:29 +0700

    ok

--- latihan01/index.php
+++ latihan01/index.php
@@ -1,6 +1,22 @@
 <?php
-	echo "<b>Hello worlds</b>";
+	echo "<b>Hello worlds</b> <hr/>";
 
 	$nama = 'Ani';
 	$nama2 = "Nur";
+	//echo $nama." , ".$nama2;
+	/** echo untuk menampilkan output */
+	echo "$nama $nama2 <br>";
+	$nilai_quis = 80;
+	$nilai_tugas = 90;
+	$hasil1= $nilai_quis + $nilai_tugas;
+	$hasil2= $nilai_quis - $nilai_tugas;
+	$hasil3= $nilai_quis * $nilai_tugas;
+	$hasil4= round($nilai_quis / $nilai_tugas);
+	$hasil5= $nilai_quis ** 2;
+	$hasil6= $nilai_quis % 2;
+	echo "penjumlahan $nilai_quis + $nilai_tugas = $hasil1 <br>";
+	echo "pengurangan $nilai_quis - $nilai_tugas = $hasil2 <br>";
+	echo "Perkalian $nilai_quis * $nilai_tugas = $hasil3 <br>";
+	echo "Pembagian $nilai_quis / $nilai_tugas = $hasil4 <br>";
+	echo "Exponen : $hasil5 , Modulus: $hasil6";
 ?>
