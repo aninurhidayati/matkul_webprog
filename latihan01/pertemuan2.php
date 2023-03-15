4f5a02f198b449d6b0280d5426a47803f9a06a1b
aninurhidayati <aninur.h@gmail.com>
2023-03-15 09:52:01 +0700

    ok

--- latihan01/pertemuan2.php
+++ latihan01/pertemuan2.php
@@ -2,13 +2,26 @@
 $pembeli = "Daud";
 $uang = 15000000;
 $laptop;
-if($uang < 10000000){
-	$laptop = "Laptop Asus";
-	$bonus ="voucher";
-}
-else
-	$laptop = "Laptop HP";
+// if($uang < 10000000){
+// 	$laptop = "Laptop Asus";
+// 	$bonus ="voucher";
+// }
+// else
+// 	$laptop = "Laptop HP";
 
+// echo "Maka $pembeli membeli $laptop";
+echo "<hr>";
+switch($uang){
+	case $uang > 10000000:
+		$laptop = "Laptop HP";
+		break;
+	case $uang > 15000000:
+		$laptop = "Laptop DELL";
+		break;	
+	default:
+		$laptop = "Laptop HP";
+		break;
+}
 echo "Maka $pembeli membeli $laptop";
 echo "<hr>";
 /*jika mahasiswa seragam lengkap maka "silahkan mengikuti kuliah"
