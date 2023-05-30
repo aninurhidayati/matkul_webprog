9e8ae86d6ffebabc763556d11428957f68bc9cbb
aninurhidayati <aninur.h@gmail.com>
2023-05-30 10:24:58 +0700

    Update proses.php

--- latihan02/admin/mod_user/proses.php
+++ latihan02/admin/mod_user/proses.php
@@ -2,16 +2,18 @@
 require_once("../../koneksidb.php");
 
 if($_GET['proses'] == "insert"){
-	echo "proses tambah";
+	
 	$usr = $_POST["user"];
 	$psw = $_POST["pass"];
 	$exsave = mysqli_query($koneksidb, 
 		"insert into mst_user(username, password) values('".$usr."', '".$psw."')")
 		or die("gagal simpan".mysqli_error($koneksidb));
+	
 	if($exsave){
 		//ketik proses simpan berhasil
 		header("Location: http://localhost/matkul_webprog/latihan02/admin/home.php?modul=mod_user");
 	}
+	
 }
 else if ($_GET['proses'] == "update"){
 	echo "proses update";
@@ -21,12 +23,11 @@ else if ($_GET['proses'] == "update"){
 	$exsave = mysqli_query($koneksidb, 
 		"UPDATE mst_user SET username='".$usr."', password='".$psw."' WHERE iduser=$id")
 		or die("gagal update".mysqli_error($koneksidb));
-
-	if($exsave){
-		//ketik proses simpan berhasil
-		header("Location: http://localhost/matkul_webprog/latihan02/admin/home.php?modul=mod_user");
-	}	
-
+	
+		if($exsave){
+			//ketik proses simpan berhasil
+			header("Location: http://localhost/matkul_webprog/latihan02/admin/home.php?modul=mod_user");
+		}
 }
 else if ($_GET['proses'] == "delete"){
 	echo "proses delete";
