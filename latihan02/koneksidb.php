f59086e50d26521fc0c5b920dc134418a0e2a258
aninurhidayati <aninur.h@gmail.com>
2023-05-30 10:25:29 +0700

    Update koneksidb.php

--- latihan02/koneksidb.php
+++ latihan02/koneksidb.php
@@ -3,4 +3,15 @@
 	$database = "db_blog";
 	$user_db = "root";
 	$pass_db = "";
+
+	//fungsi untuk mengkoneksikan ke database mysql
+	$koneksidb = mysqli_connect($servername, $user_db, $pass_db, $database);
+	if(!$koneksidb){
+		echo "Koneksi Gagal";
+		exit; //close koneksi
+	}
+	// else{
+	// 	echo "Koneksi Berhasil";
+	// }
+
 ?>
